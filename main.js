5cb601ad6d3a093b651adef9f1cef76477c6e3e5
LEGENDARY-FIRESTORM <94469035+LEGENDARY-FIRESTORM@users.noreply.github.com>
2022-12-14 18:16:24 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,67 +1,26 @@
-status = "";
-objects = [];
-alarm = "";
+status1 = "";
 
 function setup(){
     canvas = createCanvas(640, 420);
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
-    object_detector = ml5.objectDetector('cocossd', modelLoaded);
-    document.getElementById("status").innerHTML = "Status - detecting person";
 }
 
 function preload(){
-    alarm = loadSound("amoung us drip.mp3");
 }
 
 function draw(){
     image(video, 0, 0, 640, 420);
+}
 
-    if(status != ""){
-        object_detector.detect(video, gotResult);
-
-        r = random(255);
-        g = random(255);
-        b = random(255);
-
-        for(i = 0; i < objects.length; i++){
-            object_name = objects[i].label;
-            document.getElementById("status").innerHTML = "Status - detected person";
-            percent = floor(objects[i].confidence * 100);
-            fill(r, g, b);
-            noFill();
-            stroke(r, g, b);
-            text(objects[i].label + " " + percent + "%", objects[i].x, objects[i].y + 10);
-            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
-
-            if(object_name == "person"){
-                alarm.stop();
-                document.getElementById("number").innerHTML = "Baby Found";
-            }
-
-            else{
-                alarm.play();
-                document.getElementById("number").innerHTML = "Baby not Found";
-            }
-        }
-
-        if(objects.length == 0){
-            alarm.play();
-            document.getElementById("number").innerHTML = "Baby not Found";
-        }
-    }
+function start(){
+    cocossd = objectDetector("cocossd", modelLoaded);
+    inputValue = document.getElementById("input1").value;
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
 }
 
 function modelLoaded(){
-    console.log("Model is loaded");
-    status = true;
-}
-
-function gotResult(error, results){
-    if(error){
-        console.log(error);
-    }
-    console.log(results);
-    objects = results;
+    console.log("Model Loaded");
+    status1 = true;
 }
